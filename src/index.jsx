d0d05081ae871245f983911927d0453d7281d756
Abel François <abelitocoly@gmail.com>
2023-11-16 10:45:48 +0000

    Add files via upload

--- src/index.jsx
+++ src/index.jsx
@@ -7,10 +7,24 @@ import Results from './pages/Results'
 import Freelances from './pages/Freelances'
 import Header from './components/Header'
 import Error from './components/Error'
+import { createGlobalStyle } from 'styled-components'
+
+//crée un composant de style global à l'aide de la bibliothèque styled-components. Ce composant applique le style spécifié à tous les éléments div dans l'application.
+const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: 'Trebuchet MS', Helvetica, sans-serif;
+  }
+
+  body {
+    margin: 0;
+  }
+  `
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+    <GlobalStyle />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />{/* Route pour la page d'accueil */}
